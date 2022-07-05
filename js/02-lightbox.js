33bb0e9de1e3dccf3f3bef09e58bf6f213f12c91
kate-zahorui <kate.zahorui@gmail.com>
2022-07-05 21:18:25 +0300

    Task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+	.map(({ preview, original, description }) => {
+		return `<li>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>
+</li>`;
+	})
+	.join("");
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: "250ms",
+});
