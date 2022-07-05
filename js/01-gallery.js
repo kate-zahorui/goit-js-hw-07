b335d73a8e5517766db5b08855d877f0b34541f9
kate-zahorui <kate.zahorui@gmail.com>
2022-07-05 19:00:09 +0300

    Task 01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+	.map(({ preview, original, description }) => {
+		return `<div class="gallery__item">
+	<a class="gallery__link" href="${original}">
+		<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+	</a>
+</div>`;
+	})
+	.join("");
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+const onImageClick = function (event) {
+	event.preventDefault();
+
+	if (event.target.classList.contains("gallery")) {
+		return;
+	}
+
+	const source = event.target.dataset.source;
+
+	const instance = basicLightbox
+		.create(
+			`
+		<img width="1280" src="${source}">
+	`,
+		)
+		.show();
+};
+
+galleryRef.addEventListener("click", onImageClick);
